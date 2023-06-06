f6054c8d5ccbe0105a58473c97015ddd6601e967
iericksouza <erickrucke1@gmail.com>
2023-06-06 18:32:09 -0300

    estrutura

---
+++ src/anuncio.ts
@@ -0,0 +1,9 @@
+export interface Anuncio {
+  id: number;
+  titulo: string;
+  descricao: string;
+  preco: number;
+  validade: string;
+  link: string;
+  status: boolean;
+}
--- src/app/anuncio.service.ts
+++ src/app/anuncio.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Anuncio } from 'src/anuncio';
 
 @Injectable({
   providedIn: 'root'
